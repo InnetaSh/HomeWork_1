e39cefa544a63ce1def2efe50c0b2b719e0676cd
InnetaSh <inneta3@gmail.com>
2024-06-05 10:12:52 +0300

    Add task 2

--- Program.cs
+++ Program.cs
@@ -27,3 +27,63 @@ else
     Console.WriteLine("Не существует треугольник со сторонами a = {a}, b = {b}, c = {c}.\n");
 }
 #endregion
+
+#region 2
+//Дано целое число n (1 <= n <= 99), определяющее возраст человека (в годах). Для этого числа напечатать фразу «мне n лет», учитывая,
+//что при некоторых значениях n слово «лет» надо заменить на слово «год» или «года».
+int n;
+Console.WriteLine("Введите чило (количество лет, (1 <= n <= 99):");
+n = Convert.ToInt32(Console.ReadLine());
+while (n < 0 || n > 99)
+{
+    Console.WriteLine("Число вышло за пределы допустимого диапазона. Введите чило (количество лет, (1 <= n <= 99):");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 0 || (4 < n && n <= 20))
+{
+    Console.WriteLine($"Мне {n} лет.\n");
+}
+else
+{
+    if (n == 1 || n % 10 == 1)
+    {
+        Console.WriteLine($"Мне {n} год.\n");
+    }
+    else
+    {
+        if ((2 <= n && n <= 4) || ((2 <= n % 10) && (n % 10 <= 4)))
+        {
+            Console.WriteLine($"Мне {n} года.\n");
+        }
+        else
+        {
+            Console.WriteLine($"Мне {n} лет.\n");
+        }
+    }
+}
+
+//                  или (вариант 2)
+//int n;
+//Console.WriteLine("Введите чило (количество лет, (1 <= n <= 99):");
+//n = Convert.ToInt32(Console.ReadLine());
+//while (n < 0 || n > 99)
+//{
+//    Console.WriteLine("Число вышло за пределы допустимого диапазона. Введите чило (количество лет, (1 <= n <= 99):");
+//    n = Convert.ToInt32(Console.ReadLine());
+//}
+//if ((n % 10 == 0) || (((n % 100) > 10) && (n % 100) < 20) || (n % 10) > 4)
+//{
+//    Console.WriteLine($"Мне {n} лет.\n");
+//}
+//else
+//{
+//    if (n % 10 == 1)
+//    {
+//        Console.WriteLine($"Мне {n} год.\n");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Мне {n} года.\n");
+//    }
+//}
+#endregion
